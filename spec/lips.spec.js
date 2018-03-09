20c4e85057528b91db69f937f3808b6115a00f07
Jakub Jankiewicz <jcubic@onet.pl>
2018-03-09 21:16:06 +0100

    fix eslint errors

--- spec/lips.spec.js
+++ spec/lips.spec.js
@@ -266,18 +266,21 @@ describe('evaluate', function() {
                 Pair.fromArray([1, 2, 3, "foobar", 4])
             );
         });
-        it('should process multiple multiple backquote + multiple unquote', function() {
+        it('should process multiple backquote/unquote', function() {
             expect(exec('``(a ,,(+ 1 2) ,(+ 3 4))')).toEqual(
                 Pair.fromArray([
                     new Symbol('quasiquote'),
-                    [new Symbol('a'),
-                     [new Symbol('unquote'),
-                      3
-                     ],
-                     [new Symbol('+'),
-                      3,
-                      4
-                     ]
+                    [
+                        new Symbol('a'),
+                        [
+                            new Symbol('unquote'),
+                            3
+                        ],
+                        [
+                            new Symbol('+'),
+                            3,
+                            4
+                        ]
                     ]
                 ]));
         });
