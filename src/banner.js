29797f7774312241ef2968f7a401e3e3e0b0c7cc
Jakub Jankiewicz <jcubic@onet.pl>
2018-09-15 12:04:28 +0200

    missing file + run build in travis

---
+++ src/banner.js
@@ -0,0 +1,8 @@
+/**@license
+ * LIPS is Pretty Simple - version {{VER}}
+ *
+ * Copyright (c) 2018 Jakub Jankiewicz <http://jcubic.pl/me>
+ * Released under the MIT license
+ *
+ * build: {{DATE}}
+ */
