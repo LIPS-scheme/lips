e3ebc9e08a6e666fcfa1aa4e88dddc0cea95c7d5
Jakub Jankiewicz <jcubic@onet.pl>
2019-05-04 13:59:37 +0200

    3 paneles + update jquery.splitter + small fixes

--- editor/index.js
+++ editor/index.js
@@ -8,7 +8,7 @@ require('prismjs/components/prism-scheme.min');
 require('prismjs/themes/prism-coy.css');
 require('jquery.terminal/js/prism');
 require('expose-loader?git!isomorphic-git');
-require('expose-loader?LightningFS!../../lightning-fs');
+//require('expose-loader?LightningFS!../../lightning-fs');
 require('jquery.terminal/css/jquery.terminal.min.css');
 require("codemirror/lib/codemirror.css");
 require('codemirror/mode/scheme/scheme');
