b88d332a8a3ed4d38efe69563ef0d531335fa7ab
Jakub Jankiewicz <jcubic@onet.pl>
2020-11-28 18:54:22 +0100

    moving test files to tests/ dirctory

--- tests/test.js
+++ tests/test.js
@@ -15,7 +15,7 @@ const fs = require('fs');
 const readFile = promisify(fs.readFile);
 const readDir = promisify(fs.readdir);
 
-const lips = require('./src/lips');
+const lips = require('../src/lips');
 
 readDir('./tests/').then(function(filenames) {
   return Promise.all(filenames.filter(function(file) {
