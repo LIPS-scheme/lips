5460463c28ae94b94da5a9ed4fae619d49302bc5
Jakub Jankiewicz <jcubic@onet.pl>
2021-02-16 20:15:00 +0100

    add I/O example

---
+++ examples/io.scm
@@ -0,0 +1,21 @@
+(let ((fname "tmp.txt"))
+  (if (file-exists? fname)
+      (raise (string-append "file " fname " exists")))
+  (call-with-output-file fname
+    (lambda (output-port)
+      (display "hello, world" output-port)
+      (newline output-port)))
+      
+  (call-with-input-file fname
+    (lambda (input-port)
+      (let loop ((x (read-char input-port)))
+        (if (not (eof-object? x))
+            (begin
+              (display x)
+              (loop (read-char input-port)))))))
+
+  (delete-file fname))
+
+
+
+
