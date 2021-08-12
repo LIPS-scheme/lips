b0b060aee8bd3a88938854f487117ca9574289a9
Jakub Jankiewicz <jcubic@onet.pl>
2021-08-12 18:33:30 +0200

    Update code and copyright for SRFI-2

--- lib/srfi/2.scm
+++ lib/srfi/2.scm
@@ -1,40 +1,28 @@
-;; Copyright (C) Oleg Kiselyov (1998). All Rights Reserved.
+;; Copyright (C) Marc Nieper-Wißkirchen, Public Domain
 ;;
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software
-;; is furnished to do so, subject to the following conditions:
-;;
-;; The above copyright notice and this permission notice shall be included in
-;; all copies or substantial portions of the Software.
-;;
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-;; IN THE SOFTWARE.
-;;
-;; source: https://srfi.schemers.org/srfi-2/srfi-2.html
+;; ref: https://github.com/scheme-requests-for-implementation/srfi-2/tree/master/contrib
 
 (define-syntax and-let*
   (syntax-rules ()
-    ((_ ()) #t)
-    ((_ () body ...)
-     (let () body ...))
-    ((_ ((expression))) ;; last/single expression
-     expression)
-    ((_ ((symbol expression)) body ...) ;; last/single pair
-     (let ((symbol expression))
-       (if symbol (begin body ...))))
-    ((_ ((symbol expression) expr ...) body ...) ;; lead pair
-     (let ((symbol expression))
-       (and symbol (and-let* (expr ...) body ...))))
-    ((_ ((expression) expr ...) body ...) ;; lead expression
-     (and expression (and-let* (expr ...) body ...))))
+    ((_ ())
+     #t)
+    ((and-let () form form* ...)
+     (begin form form* ...))
+    ((_ ((id expr)))
+     expr)
+    ((_ ((expr)))
+     expr)
+    ((_ (id))
+     id)
+    ((_ ((id expr) . claw*) . body)
+     (let ((id expr))
+       (and id (and-let* claw* . body))))
+    ((_ ((expr) . claw*) . body)
+     (and expr (and-let* claw* . body)))
+    ((_ (id . claw*) . body)
+     (and id (and-let* claw* . body)))
+    ((_ . _)
+     (syntax-error "ill-formed and-let* form")))
   "(and-let* ((name expression) expression ...) body)
 
    Macro that combine let and and. First expression need to be in form of let.
