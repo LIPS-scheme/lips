feab1b3e3ab342ccc95f1481d574545833df9a27
Jakub Jankiewicz <jcubic@onet.pl>
2021-08-24 23:48:30 +0200

    enable macroexpand unit tests

--- tests/macroexpand.scm
+++ tests/macroexpand.scm
@@ -1,30 +1,30 @@
-(test.skip "macroexpand: should not expand let bindings"
+(test "macroexpand: should not expand let bindings"
       (lambda (t)
         (t.snapshot (macroexpand (let ((++ (lambda (a b) (* a b)))) (++ 1 2))))
         (t.snapshot (macroexpand (let ((++ 10)) (+ ++ ++))))))
 
-(test.skip "macroexpand: should only expand macro in body of let"
+(test "macroexpand: should only expand macro in body of let"
       (lambda (t)
         (define-macro (foo x) `(1 2 3 ,x))
         (define-macro (bar x) `(x y z ,x))
 
         (t.snapshot (macroexpand (let ((foo 10)) (bar foo))))))
 
-(test.skip "macroexpand: should not expand lambda parameters"
+(test "macroexpand: should not expand lambda parameters"
       (lambda (t)
         (define-macro (foo x) `(1 2 3 ,x))
         (define-macro (bar x) `(x y z ,x))
 
         (t.snapshot (macroexpand (lambda (foo) (bar foo))))))
 
-(test.skip "macroexpand: should not expand shadowed procedure name in recursive call"
+(test "macroexpand: should not expand shadowed procedure name in recursive call"
       (lambda (t)
         (define-macro (foo x) `(1 2 3 ,x))
         (define-macro (bar x) `(x y z ,x))
 
         (t.snapshot (macroexpand (define (foo x) (if (zero? x) (bar 0) (foo (- x 1))))))))
 
-(test.skip "macroexpand: should expand macro inside letrec function"
+(test "macroexpand: should expand macro inside letrec function"
       (lambda (t)
         (define-macro (foo x) `(1 2 3 ,x))
         (define-macro (bar x) `(x y z ,x))
