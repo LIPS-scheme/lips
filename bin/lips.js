8a6f7618796c53f32620654f00d6b687855c408f
Jakub Jankiewicz <jcubic@onet.pl>
2021-11-01 17:59:21 +0100

    update supported SRFI in executable

--- bin/lips.js
+++ bin/lips.js
@@ -278,7 +278,7 @@ if (options.version || options.V) {
         ["website", "https://lips.js.org"],
         ['languages', 'scheme', 'r5rs', 'r7rs'].map(LSymbol),
         ['encodings', 'utf-8'].map(LSymbol),
-        ["scheme.srfi", 0, 4, 6, 10, 22, 23, 46, 176],
+        ["scheme.srfi", 0, 1, 2, 4, 6, 8, 10, 22, 23, 26, 46, 69, 98, 111, 156, 176],
         ["release", version],
         ["os.uname", os.platform(), os.release()],
         ["os.env.LANG", process.env.LANG],
