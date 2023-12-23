cc5fdfb61f476c541d145637cef9a00b33780eeb
Jakub Jankiewicz <jcubic@onet.pl>
2023-12-23 14:42:01 +0100

    update version

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: publish test coveralls lint zero coverage
 
-VERSION=1.0.0-beta.16
-VERSION_DASH=`echo -n "1.0.0-beta.16" | sed "s/-/%E2%80%93/"`
+VERSION=1.0.0-beta.17
+VERSION_DASH=`echo -n "1.0.0-beta.17" | sed "s/-/%E2%80%93/"`
 BRANCH=`git branch | grep '^*' | sed 's/* //'`
 DATE=`date -uR`
 YEAR=`date +%Y`
--- dist/lips.js
+++ dist/lips.js
@@ -4,7 +4,7 @@
  * | |   \ \     | |  | || . \/ __>  | |
  * | |    > \    | |_ | ||  _/\__ \  | |
  * | |   / ^ \   |___||_||_|  <___/  | |
- *  \_\ /_/ \_\                     /_/ v. DEV
+ *  \_\ /_/ \_\                     /_/ v. 1.0.0-beta.17
  *
  * LIPS is Pretty Simple - Scheme based Powerful LISP in JavaScript
  *
@@ -31,7 +31,7 @@
  * Copyright (c) 2014-present, Facebook, Inc.
  * released under MIT license
  *
- * build: Wed, 22 Nov 2023 13:53:23 +0000
+ * build: Sat, 23 Dec 2023 13:40:35 +0000
  */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -5799,7 +5799,7 @@
    * The rationalize algorithm is by Per M.A. Bothner, Alan Bawden and Marc Feeley.
    * source: Kawa, C-Gambit
    *
-   * Build time: Wed, 22 Nov 2023 13:53:23 +0000
+   * Build time: Sat, 23 Dec 2023 13:40:35 +0000
    */
   var _excluded = ["token"],
       _excluded2 = ["stderr", "stdin", "stdout", "command_line"];
@@ -19582,10 +19582,10 @@
 
   var banner = function () {
     // Rollup tree-shaking is removing the variable if it's normal string because
-    // obviously 'Wed, 22 Nov 2023 13:53:23 +0000' == '{{' + 'DATE}}'; can be removed
+    // obviously 'Sat, 23 Dec 2023 13:40:35 +0000' == '{{' + 'DATE}}'; can be removed
     // but disabling Tree-shaking is adding lot of not used code so we use this
     // hack instead
-    var date = LString('Wed, 22 Nov 2023 13:53:23 +0000').valueOf();
+    var date = LString('Sat, 23 Dec 2023 13:40:35 +0000').valueOf();
 
     var _date = date === '{{' + 'DATE}}' ? new Date() : new Date(date);
 
@@ -19597,7 +19597,7 @@
 
     var _build = [_year, _format(_date.getMonth() + 1), _format(_date.getDate())].join('-');
 
-    var banner = "\n  __ __                          __\n / / \\ \\       _    _  ___  ___  \\ \\\n| |   \\ \\     | |  | || . \\/ __>  | |\n| |    > \\    | |_ | ||  _/\\__ \\  | |\n| |   / ^ \\   |___||_||_|  <___/  | |\n \\_\\ /_/ \\_\\                     /_/\n\nLIPS Interpreter DEV (".concat(_build, ") <https://lips.js.org>\nCopyright (c) 2018-").concat(_year, " Jakub T. Jankiewicz\n\nType (env) to see environment with functions macros and variables. You can also\nuse (help name) to display help for specific function or macro, (apropos name)\nto display list of matched names in environment and (dir object) to list\nproperties of an object.\n").replace(/^.*\n/, '');
+    var banner = "\n  __ __                          __\n / / \\ \\       _    _  ___  ___  \\ \\\n| |   \\ \\     | |  | || . \\/ __>  | |\n| |    > \\    | |_ | ||  _/\\__ \\  | |\n| |   / ^ \\   |___||_||_|  <___/  | |\n \\_\\ /_/ \\_\\                     /_/\n\nLIPS Interpreter 1.0.0-beta.17 (".concat(_build, ") <https://lips.js.org>\nCopyright (c) 2018-").concat(_year, " Jakub T. Jankiewicz\n\nType (env) to see environment with functions macros and variables. You can also\nuse (help name) to display help for specific function or macro, (apropos name)\nto display list of matched names in environment and (dir object) to list\nproperties of an object.\n").replace(/^.*\n/, '');
     return banner;
   }(); // -------------------------------------------------------------------------
   // to be used with string function when code is minified
@@ -19629,9 +19629,9 @@
   read_only(QuotedPromise, '__class__', 'promise'); // -------------------------------------------------------------------------
 
   var lips = {
-    version: 'DEV',
+    version: '1.0.0-beta.17',
     banner: banner,
-    date: 'Wed, 22 Nov 2023 13:53:23 +0000',
+    date: 'Sat, 23 Dec 2023 13:40:35 +0000',
     exec: exec,
     // unwrap async generator into Promise<Array>
     parse: compose(uniterate_async, parse),
