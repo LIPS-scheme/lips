d0eafce65c5a7239584ba5ceaff60ef4d05d5df2
Jakub Jankiewicz <jcubic@onet.pl>
2024-01-07 17:53:19 +0100

    remove invalid unit test for call/cc

--- tests/continuations.scm
+++ tests/continuations.scm
@@ -1,14 +1,3 @@
-(test.failing "continuations: base"
-      (lambda (t)
-        (define x 0)
-
-        (t.is (+ 2 (call/cc (lambda (cc)
-                              (set! x cc)
-                              3)))
-              5)
-
-        (t.is (x 4) 6)))
-
 (test.failing "continuations: return"
       (lambda (t)
         (let ((called #f))
