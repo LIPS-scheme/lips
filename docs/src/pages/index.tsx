4ef43613dd036bd65ef6609fccc508285b5582c0
Jakub Jankiewicz <jcubic@onet.pl>
2024-02-11 01:48:18 +0100

    add LIPS version

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useRef, useEffect } from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
@@ -11,11 +12,21 @@ import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const ref = useRef<HTMLSpanElement>();
+  useEffect(() => {
+    (function loop() {
+      if (!(window as any).lips) {
+        setTimeout(loop, 100);
+      } else {
+        ref.current.innerText = (window as any).lips.version;
+      }
+    })();
+  }, []);
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          {siteConfig.title} v.<span ref={ref}></span>
         </Heading>
         <Interpreter/>
       </div>
