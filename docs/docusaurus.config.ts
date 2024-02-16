5ef7634041156bca2921ddd39d46eafb23972b41
Jakub Jankiewicz <jcubic@onet.pl>
2024-02-16 23:26:21 +0100

    update edit github links

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,14 +40,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jcubic/lips-website/tree/docusaurus/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jcubic/lips-website/tree/docusaurus/docs/',
         },
         theme: {
           customCss: './src/css/custom.css',
