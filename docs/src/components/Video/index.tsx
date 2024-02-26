4da172ff9226bab3a9402461c791c1e0396fd3c6
Jakub Jankiewicz <jcubic@onet.pl>
2024-02-26 19:08:40 +0100

    update video description

--- docs/src/components/Video/index.tsx
+++ docs/src/components/Video/index.tsx
@@ -7,9 +7,7 @@ export default function Video(): JSX.Element {
     <div className={styles.video}>
       <Heading as="h2" className={styles.header}>Video Presentation about LIPS Scheme</Heading>
       <p>
-        Video presentation given during <a href="https://archive.fosdem.org/2023/">FOSDEM
-        2023</a>. It discuss different aspect of LIPS Scheme. It first gives quick intro to Lisp and
-        Scheme and later show diffrent features of LIPS Scheme.
+        Video presentation given during <a href="https://archive.fosdem.org/2023/">FOSDEM 2023</a> in <a href="https://archive.fosdem.org/2023/schedule/track/declarative_and_minimalistic_computing/">Declarative and Minimalistic Computing devroom</a>. It discuss different aspect of LIPS Scheme. It first gives quick intro to Lisp and Scheme and later show diffrent features of LIPS Scheme.
       </p>
       <video controls preload="none" poster="./img/fosdem-intro.png">
         <source src="https://video.fosdem.org/2023/D.minimalistic/lipsscheme.webm" type="video/webm; codecs=&quot;vp9, opus&quot;"/>
