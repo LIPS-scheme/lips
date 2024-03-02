53e5c8cfe8a65176d937e453c493f0388ba0f262
Jakub Jankiewicz <jcubic@onet.pl>
2024-03-02 13:11:05 +0100

    add analytics

---
+++ docs/src/theme/Footer/Layout/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import clsx from 'clsx';
+export default function FooterLayout({style, links, logo, copyright}) {
+  return (
+    <footer
+      className={clsx('footer', {
+        'footer--dark': style === 'dark',
+      })}>
+      <div className="container container-fluid">
+        {links}
+        {(logo || copyright) && (
+          <div className="footer__bottom text--center">
+            {logo && <div className="margin-bottom--sm">{logo}</div>}
+            {copyright}
+          </div>
+        )}
+      </div>
+      <script>
+        {`
+var owa_baseUrl = 'https://stats.jcubic.pl/';
+var owa_cmds = owa_cmds || [];
+owa_cmds.push(['setSiteId', '922150e605063a8580e17047ffbf3ec2']);
+owa_cmds.push(['trackPageView']);
+owa_cmds.push(['trackClicks']);
+
+(function() {
+    var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true      owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+    _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+    var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+}());`}
+      </script>
+    </footer>
+  );
+}
