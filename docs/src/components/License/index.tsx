ca839b9b79d8da5b6924e53d0a25eb54bbf53937
Jakub Jankiewicz <jcubic@onet.pl>
2024-03-02 19:32:39 +0100

    fix grammar mistake

--- docs/src/components/License/index.tsx
+++ docs/src/components/License/index.tsx
@@ -7,7 +7,7 @@ export default function License(): JSX.Element {
         <Heading as="h2" className={styles.header}>License</Heading>
         <p>
           LIPS Scheme is <a href="https://opensource.com/resources/what-open-source">Open Source</a>
-          {' '}is released on <a href="https://github.com/jcubic/lips/blob/master/LICENSE">MIT license</a></p>
+          {' '}and released on <a href="https://github.com/jcubic/lips/blob/master/LICENSE">MIT license</a></p>
       </div>
     );
 }
