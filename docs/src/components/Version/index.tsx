40266ca48344a08a53e17c03d8eaa4cd6f181ddc
Jakub Jankiewicz <jcubic@onet.pl>
2024-03-19 13:04:54 +0100

    make LIPS version on home page static

--- docs/src/components/Version/index.tsx
+++ docs/src/components/Version/index.tsx
@@ -1,26 +1,5 @@
-import { useEffect, useState } from 'react';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import version from '@site/version.json';
 
-function Version(): JSX.Element {
-    const [version, setVersion] = useState<string>('');
-    useEffect(() => {
-        (function delay() {
-            if ((window as any).lips == undefined) {
-                setTimeout(delay, 50);
-            } else {
-                setVersion((window as any).lips.version);
-            }
-        })();
-    }, []);
+export default function Version(): JSX.Element {
     return <span>{version}</span>;
-}
-
-export default function BrowserVersion(): JSX.Element {
-  return (
-    <BrowserOnly fallback={<span>...</span>}>
-      {() => {
-        return <Version/>
-      }}
-    </BrowserOnly>
-  );
 }
